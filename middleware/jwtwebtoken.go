bc5f63df5d161805fcd343c7d074ef125ab254cc
Xenopartyyy <willyawan.m@gmail.com>
2023-10-19 13:45:11 +0700

    [FEAT] Adding role field for generating token

--- middleware/jwtwebtoken.go
+++ middleware/jwtwebtoken.go
@@ -10,11 +10,12 @@ import (
 var secretkey = "rahasia"
 var err error
 
-func GenerateToken(ID uint, Email string) (string, error) {
+func GenerateToken(ID uint, Email string, Role string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":    ID,
 		"email": Email,
+		"role":  Role,
 	})
 
 	signedToken, err := token.SignedString([]byte(secretkey))
