4d9ce881bede45cd1daca4b3611d2047e30a6e22
Xenopartyyy <willyawan.m@gmail.com>
2023-10-21 11:42:30 +0700

    [FIX] Restructuring dto

--- dto/categorydto.go
+++ dto/categorydto.go
@@ -1,11 +1,15 @@
 package dto
 
 import (
+	"FP3-Hacktiv8/model"
+	_ "FP3-Hacktiv8/model"
 	"time"
 )
 
 type Category struct {
 	ID        uint      `json:"id"`
 	Type      string    `json:"type"`
+	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
+	Tasks     []model.Task
 }
