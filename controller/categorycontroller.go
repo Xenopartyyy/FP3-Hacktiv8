d0722bf6fea5382edc54364f2c71a239202a5b60
Xenopartyyy <willyawan.m@gmail.com>
2023-10-21 11:41:58 +0700

    [FEAT] Add Create Patch and Delete for category

--- controller/categorycontroller.go
+++ controller/categorycontroller.go
@@ -5,27 +5,13 @@ import (
 	db "FP3-Hacktiv8/infra/database"
 	"FP3-Hacktiv8/model"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
 )
 
-func Create(c *gin.Context) {
+func CreateCategory(c *gin.Context) {
 	var category model.Category
-	userData, _ := c.Get("userData")
-	userClaim, ok := userData.(jwt.MapClaims)
-
-	if !ok {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid token"})
-		return
-	}
-
-	userROLE := userClaim["role"].(string)
-
-	if userROLE != "admin" {
-		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "You don't have permission to perform this action"})
-		return
-	}
 
 	if err := c.ShouldBindJSON(&category); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
@@ -44,4 +30,70 @@ func Create(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, categorydto)
+}
+
+// func GetCategory(c *gin.Context) {
+// 	var category model.Category
+
+// 	if err := db.DB.Find(category); err != nil {
+// 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Data doesnt exist!"})
+// 	}
+
+// 	response := dto.Category{}
+
+// 	for_, category := range err
+
+// 	c.JSON(http.StatusOK, categorydto)
+// }
+
+func PatchCategory(c *gin.Context) {
+	categoryID, err := strconv.Atoi(c.Param("categoryID"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Invalid categoryID"})
+		return
+	}
+	var category model.Category
+
+	if err := c.ShouldBindJSON(&category); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	if db.DB.Model(&category).Where("id = ?", categoryID).Updates(&category).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Failed to update category"})
+		return
+	}
+
+	userdto := dto.Category{
+		ID:        uint(categoryID),
+		Type:      category.Type,
+		CreatedAt: category.UpdatedAt,
+	}
+
+	c.JSON(http.StatusOK, userdto)
+}
+
+func DeleteCategory(c *gin.Context) {
+
+	categoryID, err := strconv.Atoi(c.Param("categoryID"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Invalid categoryID"})
+		return
+	}
+
+	var category model.Category
+
+	if err := db.DB.Where("id = ?", categoryID).Find(&category).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Category not found"})
+		return
+	}
+
+	if err := db.DB.Delete(&category, categoryID).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Failed to delete category"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Category has been succesfully deleted",
+	})
 }
