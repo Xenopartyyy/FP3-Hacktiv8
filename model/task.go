fb5bb3ebeaa1f6fcc38a64dc32567a448357d28f
Xenopartyyy <willyawan.m@gmail.com>
2023-10-23 21:28:43 +0700

    [FIX] Editing validation

--- model/task.go
+++ model/task.go
@@ -6,8 +6,8 @@ import (
 
 type Task struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
-	Title       string `gorm:"type:varchar(100)" json:"title" validate:"required"`
-	Description string `gorm:"type:varchar(50)" json:"description" validate:"required"`
+	Title       string `gorm:"type:varchar(50)" json:"title" validate:"required"`
+	Description string `gorm:"type:varchar(100)" json:"description" validate:"required"`
 	Status      bool   `json:"status" validate:"boolean"`
 	UserID      uint   `json:"user_id"`
 	CategoryID  uint   `json:"category_id"`
