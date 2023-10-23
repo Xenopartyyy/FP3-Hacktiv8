5259ec81fcc46ad4122d5e9486ff074fb9d3d908
Xenopartyyy <willyawan.m@gmail.com>
2023-10-23 19:43:34 +0700

    [FEAT] Add few DTO

--- dto/taskdto.go
+++ dto/taskdto.go
@@ -23,3 +23,20 @@ type UpdateTask struct {
 	CategoryID  uint      `json:"category_id"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+type GetTask struct {
+	ID          uint      `json:"id"`
+	Title       string    `json:"title"`
+	Status      bool      `json:"status"`
+	Description string    `json:"description"`
+	UserID      uint      `json:"user_id"`
+	CategoryID  uint      `json:"category_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	User        []GetUserTask
+}
+
+type GetUserTask struct {
+	ID        uint   `json:"id"`
+	Email     string `json:"email"`
+	Full_name string `json:"full_name"`
+}
